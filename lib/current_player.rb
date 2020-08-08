0e1a5f06d723ae6b6c3bec573186b77df63125a1
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 03:03:45 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,30 @@
 # how many turns have been played
 def turn_count(board)
   count = 0
+<<<<<<< HEAD
   board.each do |player|
     if player == "X" || player == "O"
       count += 1
     end
   end
   return count
+=======
+  # loop over elements of board array
+  while count < 10 |
+    if board[count] = "X" || board[count] == "O"
+      count += 1 
+    end
+  end
+>>>>>>> eea6c4faab0d64c539fc2f659deaffa5fa477e2c
 end
 
 # returns X if user's turn, O if it's computer's turn
 def current_player(board)
+<<<<<<< HEAD
   if turn_count(board) % 2 == 0 
+=======
+  if turn_count % 2 == 0 
+>>>>>>> eea6c4faab0d64c539fc2f659deaffa5fa477e2c
     "X"
   else
     "O"
